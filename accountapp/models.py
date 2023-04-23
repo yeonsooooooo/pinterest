5c16e884fb3d3e6d3a3fc5832ccaab0b5cbe3388
yeonsoo <yskim80512@naver.com>
2023-04-23 23:14:48 +0900

    django course 14 commit

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# python manage.py makemigrations
+# 우리가 models.py에 작성하는 python file을 database와 연결하기 위한 파일로 변환시켜 준다
+#python manage.py migrate
+#database로 연결 시켜주기 위한 파일을 만들었다면, 그 이후에는 그 파일을 연결 시켜주는 명령어 필요.
+#그게 위 명령어
+
+class HelloWorld(models.Model):
+    text = models.CharField(max_length= 255, null=False)
