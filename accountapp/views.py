cad0382f2185d31c7d2a8cb59c8af38536ea47f3
yeonsoo <yskim80512@naver.com>
2023-04-25 16:49:54 +0900

    django course 18 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from accountapp.models import HelloWorld
+
 
 # Create your views here.
 
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'accountapp/helloworld.html', context={'text':'POST METHOD !!'})
+
+        temp = request.POST.get('hello_world_input')
+        #
+        # 가져온 데이터를 저장하는 법
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        #데이터베이스에 정보를 가져와서 출력하는 법
+        hello_world_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
+        # return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
