edea6abe3edce5c318d262d7f0db4f04cf96bfb0
yeonsoo <yskim80512@naver.com>
2023-04-26 22:56:58 +0900

    django course 24 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accountapp.models import HelloWorld
 
@@ -37,3 +37,9 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+class AccountDetailView(DetailView):
+    model = User
+    template_name = 'accountapp/detail.html'
+    # 방문한 사람의 정보가 아니라, 특정 pk를 가진 사람의 정보를 보여주려면 view에서 제공하는 context_object_name 을 써주면 된다.
+    context_object_name = 'target_user'
