ba5e3914d119639065d1a092ef3f09c945ca8f4a
yeonsoo <yskim80512@naver.com>
2023-05-02 02:00:15 +0900

    django 30 commit

--- progmatic/settings.py
+++ progmatic/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accountapp',
-    'bootstrap4'
+    'bootstrap4',
+    'profileapp',
+
 ]
 
 MIDDLEWARE = [
