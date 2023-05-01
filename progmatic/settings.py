39fe481066034744db1b562ccfa08875ac68a2f6
yeonsoo <yskim80512@naver.com>
2023-05-02 00:58:38 +0900

    django 29 commit

--- progmatic/settings.py
+++ progmatic/settings.py
@@ -134,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
